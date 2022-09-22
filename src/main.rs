50db946ff59341bf4d1601d1ec4415bd6d415872
elsueter <joseph_straw@outlook.com>
2022-09-22 19:20:48 +0100

    Implemented reference based tree

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,58 @@
 mod tree;
 
 fn main() {
+
+    // // F(A) = !B & C | D
+    // let node_a = tree::BooleanTree::new(
+    //     tree::and_node(
+    //         tree::not_val_node(false, 1),
+    //         tree::or_node(
+    //             tree::val_node(false, 2),
+    //             tree::val_node(false, 3)
+    //         )
+    //     )
+    // );
+
+    // // F(B) = A & D
+    // let node_b = tree::BooleanTree::new(
+    //     tree::and_node(
+    //         tree::val_node(false, 0),
+    //         tree::val_node(false, 3)
+    //     )
+    // );
+
+    // // F(C) = B
+    // let node_c = tree::BooleanTree::new(
+    //     tree::val_node(false, 1)
+    // );
+
+    // // F(D) = !A | D
+    // let node_d = tree::BooleanTree::new(
+    //     tree::or_node(
+    //         tree::not_val_node(false, 0),
+    //         tree::val_node(false, 3)
+    //     )
+    // );
+    
+    let x = &tree::not_val_node(false, 1);
+    let y = &tree::val_node(false, 2);
+    let z = &tree::val_node(false, 3);
+    let x1 = &tree::or_node(
+        y,
+        z
+    );
     // F(A) = !B & C | D
     let node_a = tree::BooleanTree::new(
         tree::and_node(
-            tree::not_val_node(false, 1),
-            tree::or_node(
-                tree::val_node(false, 2),
-                tree::val_node(false, 3)
-            )
+            x,
+            x1
         )
     );
 
-    // F(B) = A & D
-    let node_b = tree::BooleanTree::new(
-        tree::and_node(
-            tree::val_node(false, 0),
-            tree::val_node(false, 3)
-        )
-    );
+    let mut temp: Vec<tree::BooleanTree<bool>> = Vec::from([node_a]);
 
-    // F(C) = B
-    let node_c = tree::BooleanTree::new(
-        tree::val_node(false, 1)
-    );
-
-    // F(D) = !A | D
-    let node_d = tree::BooleanTree::new(
-        tree::or_node(
-            tree::not_val_node(false, 0),
-            tree::val_node(false, 3)
-        )
-    );
-
-    let mut temp: Vec<tree::BooleanTree<bool>> = Vec::from([node_a, node_b, node_c, node_d]);
-
-    use std::time::Instant;
-    let now = Instant::now();
-
-    for i in 0..1000{
-        for node in temp.iter(){
-            println!("{}", node.resolve());
-        }
-    }
-
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
+    for node in temp.iter(){
+        node.resolve();
+    };
 
 }
--- src/tree.rs
+++ src/tree.rs
@@ -1,51 +1,43 @@
-#[derive(Clone)]
 pub enum Op<T>{
     And,
     Or,
     Val(T)
 }
 
-#[derive(Clone)]
-pub struct Node<T>{
+pub struct Node<'a, T>{
     op: Op<T>,
-    left: Option<Box<Node<T>>>,
-    right: Option<Box<Node<T>>>,
+    left: Option<Box<&'a Node<'a, T>>>,
+    right: Option<Box<&'a Node<'a, T>>>,
     name: Option<u8>
 }
 
-impl Node<bool>{
-    pub fn new(in_op: Op<bool>, in_left: Node<bool>, in_right: Node<bool>) -> Node<bool> {
-        Node{op: in_op, left: Some(Box::new(in_left)), right: Some(Box::new(in_right)), name: None}
-    }
-}
+impl Node<'_, bool>{}
 
-pub fn and_node(l: Node<bool>, r: Node<bool>) -> Node<bool> {Node::new(Op::And, l, r)}
-pub fn or_node(l: Node<bool>, r: Node<bool>) -> Node<bool> {Node::new(Op::Or, l, r)}
-pub fn val_node(in_val: bool, in_name: u8) -> Node<bool> {Node::<bool>{op: Op::Val(in_val), left: None, right: None, name: Some(in_name)}}
-pub fn not_val_node(in_val: bool, in_name: u8) -> Node<bool> {Node::<bool>{op: Op::Val(!in_val), left: None, right: None, name: Some(in_name)}}
+pub fn and_node<'a>(l: &'a Node<'a, bool>, r: &'a Node<'a, bool>) -> Node<'a, bool> {Node::<bool>{op: Op::And, left: Some(Box::new(l)), right: Some(Box::new(r)), name: None}}
+pub fn or_node<'a>(l: &'a Node<'a, bool>, r: &'a Node<'a, bool>) -> Node<'a, bool> {Node::<bool>{op: Op::Or, left: Some(Box::new(l)), right: Some(Box::new(r)), name: None}}
+pub fn val_node(in_val: bool, in_name: u8) -> Node<'static, bool> {Node::<bool>{op: Op::Val(in_val), left: None, right: None, name: Some(in_name)}}
+pub fn not_val_node(in_val: bool, in_name: u8) -> Node<'static, bool> {Node::<bool>{op: Op::Val(!in_val), left: None, right: None, name: Some(in_name)}}
 
-pub struct BooleanTree<T>{root: Node<T>}
+pub struct BooleanTree<'a, T>{root: Node<'a, T>}
 
-impl BooleanTree<bool>{
+impl BooleanTree<'_, bool>{
     pub fn new(in_node: Node<bool>) -> BooleanTree<bool> {BooleanTree{root: in_node}}
 
     pub fn resolve(&self) -> bool {
-        Self::resolve_node(Box::new(&self.root))
+        Self::resolve_node(&Box::new(&self.root))
     }
 
-    fn resolve_node(cur_node: Box<&Node<bool>>) -> bool {
+    fn resolve_node(cur_node: &Box<&Node<bool>>) -> bool {
         let mut l: Option<bool> = None;
         let mut r: Option<bool> = None;
 
-        //TODO - see about removing need for let and Box::new -> extra memory usage
         if let Some(left) = &cur_node.left {
-            l = Some(BooleanTree::resolve_node(Box::new(&(*left))));
+            l = Some(BooleanTree::resolve_node(left));
         }
         if let Some(right) = &cur_node.right {
-            r = Some(BooleanTree::resolve_node(Box::new(&(*right))));
+            r = Some(BooleanTree::resolve_node(right));
         }
 
-        //TODO - see if nicer way to tell if Op is filled in -> extra memory usage
         let l: bool = if let Some(x) = l{x} else{false};
         let r: bool = if let Some(x) = r{x} else{false};
 
