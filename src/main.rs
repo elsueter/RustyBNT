fb4365984139e35461251f3ecf26219c469600f7
elsueter <joseph_straw@outlook.com>
2022-09-25 04:48:54 +0100

    Implemented tree print

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,16 @@ mod slow_tree;
 
 fn main() {
 
-    // F(A) = !B & C | D
-    let node_a_slow = slow_tree::BooleanTree::new(
-        slow_tree::and_node(
-            slow_tree::not_val_node(false, 1),
-            slow_tree::or_node(
-                slow_tree::val_node(false, 2),
-                slow_tree::val_node(false, 3)
-            )
-        )
-    );
-
-    let temp_1: Vec<slow_tree::BooleanTree<bool>> = Vec::from([node_a_slow]);
+    // // F(A) = !B & C | D
+    // let node_a_slow = slow_tree::BooleanTree::new(
+    //     slow_tree::and_node(
+    //         slow_tree::not_val_node(false, 1),
+    //         slow_tree::or_node(
+    //             slow_tree::val_node(false, 2),
+    //             slow_tree::val_node(false, 3)
+    //         )
+    //     )
+    // );
 
     // // F(B) = A & D
     // let node_b = tree::BooleanTree::new(
@@ -37,43 +35,27 @@ fn main() {
     //     )
     // );
     
-    let x = &tree::not_val_node(false, 1);
-    let y = &tree::val_node(false, 2);
+    let x = &tree::not_val_node(true, 1);
+    let y = &tree::val_node(true, 2);
     let z = &tree::val_node(false, 3);
     let x1 = &tree::or_node(
         y,
         z
     );
+    let x2 = &tree::and_node(
+        x1,
+        x1
+    );
     // F(A) = !B & C | D
-    let node_a = tree::BooleanTree::new(
-        tree::and_node(
-            x,
-            x1
+    let mut node_a = Box::new(
+        tree::BooleanTree::new(
+            tree::and_node(
+                x2,
+                x1
+            )
         )
     );
 
-    let temp_2: Vec<tree::BooleanTree<bool>> = Vec::from([node_a]);
-
-    use std::time::Instant;
-    let now = Instant::now();
-
-    for i in 1..1000000{
-        for node in temp_1.iter(){
-            node.resolve();
-        };
-    }
-
-    let elapsed = now.elapsed();
-    println!("Slow elapsed: {:.2?}", elapsed);
-    let now = Instant::now();
-
-    for i in 1..1000000{
-        for node in temp_2.iter(){
-            node.resolve();
-        };
-    }
-
-    let elapsed = now.elapsed();
-    println!("Normal elapsed: {:.2?}", elapsed);
+    node_a.print_tree();
 
 }
--- src/tree.rs
+++ src/tree.rs
@@ -11,16 +11,81 @@ pub struct Node<'a, T>{
     name: Option<u8>
 }
 
-impl Node<'_, bool>{}
+impl Node<'_, bool>{
+    fn print(&self, mut buffer: Vec<String>, layer: usize, lr: bool) -> Vec<String>{
 
-pub fn and_node<'a>(l: &'a Node<'a, bool>, r: &'a Node<'a, bool>) -> Node<'a, bool> {Node::<bool>{op: Op::And, left: Some(Box::new(l)), right: Some(Box::new(r)), name: None}}
-pub fn or_node<'a>(l: &'a Node<'a, bool>, r: &'a Node<'a, bool>) -> Node<'a, bool> {Node::<bool>{op: Op::Or, left: Some(Box::new(l)), right: Some(Box::new(r)), name: None}}
-pub fn val_node(in_val: bool, in_name: u8) -> Node<'static, bool> {Node::<bool>{op: Op::Val(in_val), left: None, right: None, name: Some(in_name)}}
-pub fn not_val_node(in_val: bool, in_name: u8) -> Node<'static, bool> {Node::<bool>{op: Op::Val(!in_val), left: None, right: None, name: Some(in_name)}}
+        let mut curr = "".to_string();
+
+        if(buffer.len() < layer+1){
+            buffer.push("".to_string());
+        }
+        
+        if(lr){
+            curr.push_str(&"|-".to_string())
+        }else{
+            curr.push_str(&"\\-".to_string())
+        }
+
+        match self.op{
+            Op::And => {
+                curr.push_str(&"And ".to_string());
+                if(layer > 0){
+                    buffer[layer-1].push_str(&"  ".to_string());
+                }
+            },
+            Op::Or => {
+                curr.push_str(&"Or  ".to_string());
+                if(layer > 0){
+                    buffer[layer-1].push_str(&"   ".to_string());
+                }
+            },
+            Op::Val(x) => {
+                if let Some(value) = self.name{ 
+                    curr.push_str(&format!("{}", value));
+                }else{
+                    curr.push_str(&"".to_string());
+                }
+                if(lr){
+                    if(buffer.len() < layer+2){
+                        buffer.push("   ".to_string());
+                    }else{
+                        buffer[layer+1].push_str(&"   ".to_string());
+                    }
+                }
+            }
+        }
+        buffer[layer].push_str(&curr);
+
+        //TODO - see about new implementation without needing `let`
+        if let Some(left) = &self.left {
+            buffer = left.print(buffer, layer+1, true);
+        }
+        //TODO - see about new implementation without needing `let`
+        if let Some(right) = &self.right {
+            buffer = right.print(buffer, layer+1, false);
+        }
+
+        return buffer;
+    }
+}
+
+pub fn and_node<'a>(l: &'a Node<'a, bool>, r: &'a Node<'a, bool>) -> Node<'a, bool> {
+    Node::<bool>{op: Op::And, left: Some(Box::new(l)), right: Some(Box::new(r)), name: None}
+}
+pub fn or_node<'a>(l: &'a Node<'a, bool>, r: &'a Node<'a, bool>) -> Node<'a, bool> {
+    Node::<bool>{op: Op::Or, left: Some(Box::new(l)), right: Some(Box::new(r)), name: None}
+}
+pub fn val_node(in_val: bool, in_name: u8) -> Node<'static, bool> {
+    Node::<bool>{op: Op::Val(in_val), left: None, right: None, name: Some(in_name)}
+}
+pub fn not_val_node(in_val: bool, in_name: u8) -> Node<'static, bool> {
+    Node::<bool>{op: Op::Val(!in_val), left: None, right: None, name: Some(in_name)}
+}
 
 pub struct BooleanTree<'a, T>{root: Node<'a, T>}
 
 impl BooleanTree<'_, bool>{
+
     pub fn new(in_node: Node<bool>) -> BooleanTree<bool> {BooleanTree{root: in_node}}
 
     pub fn resolve(&self) -> bool {
@@ -31,14 +96,18 @@ impl BooleanTree<'_, bool>{
         let mut l: Option<bool> = None;
         let mut r: Option<bool> = None;
 
+        //TODO - see about new implementation without needing `let`
         if let Some(left) = &cur_node.left {
             l = Some(BooleanTree::resolve_node(left));
         }
+        //TODO - see about new implementation without needing `let`
         if let Some(right) = &cur_node.right {
             r = Some(BooleanTree::resolve_node(right));
         }
 
+        //TODO - see about new implementation without needing `let`
         let l: bool = if let Some(x) = l{x} else{false};
+        //TODO - see about new implementation without needing `let`
         let r: bool = if let Some(x) = r{x} else{false};
 
         match cur_node.op{
@@ -47,4 +116,17 @@ impl BooleanTree<'_, bool>{
             Op::Val(x) => x
         }
     }
+
+    pub fn update_state(in_state: &Vec<bool>){
+
+    }
+
+    pub fn print_tree(&self){
+        let mut buffer = Vec::new();
+        buffer = self.root.print(buffer, 0, true);
+        for layer in buffer.iter(){
+            println!("{}", layer);
+        }
+    }
+
 }
