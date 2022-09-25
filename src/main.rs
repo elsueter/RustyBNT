d2fcb5709978401c9d1414d5770cc3a90c3459b1
elsueter <joseph_straw@outlook.com>
2022-09-25 01:52:35 +0100

    Added tests for tree

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 mod tree;
+mod slow_tree;
 
 fn main() {
 
-    // // F(A) = !B & C | D
-    // let node_a = tree::BooleanTree::new(
-    //     tree::and_node(
-    //         tree::not_val_node(false, 1),
-    //         tree::or_node(
-    //             tree::val_node(false, 2),
-    //             tree::val_node(false, 3)
-    //         )
-    //     )
-    // );
+    // F(A) = !B & C | D
+    let node_a_slow = slow_tree::BooleanTree::new(
+        slow_tree::and_node(
+            slow_tree::not_val_node(false, 1),
+            slow_tree::or_node(
+                slow_tree::val_node(false, 2),
+                slow_tree::val_node(false, 3)
+            )
+        )
+    );
+
+    let temp_1: Vec<slow_tree::BooleanTree<bool>> = Vec::from([node_a_slow]);
 
     // // F(B) = A & D
     // let node_b = tree::BooleanTree::new(
@@ -49,10 +52,28 @@ fn main() {
         )
     );
 
-    let mut temp: Vec<tree::BooleanTree<bool>> = Vec::from([node_a]);
+    let temp_2: Vec<tree::BooleanTree<bool>> = Vec::from([node_a]);
 
-    for node in temp.iter(){
-        println!("{}", node.resolve());
-    };
+    use std::time::Instant;
+    let now = Instant::now();
+
+    for i in 1..1000000{
+        for node in temp_1.iter(){
+            node.resolve();
+        };
+    }
+
+    let elapsed = now.elapsed();
+    println!("Slow elapsed: {:.2?}", elapsed);
+    let now = Instant::now();
+
+    for i in 1..1000000{
+        for node in temp_2.iter(){
+            node.resolve();
+        };
+    }
+
+    let elapsed = now.elapsed();
+    println!("Normal elapsed: {:.2?}", elapsed);
 
 }
